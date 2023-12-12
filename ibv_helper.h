93a06084e28aca0701107e362b2b42fd6583dcf1
yusen <61572100+yslys@users.noreply.github.com>
2023-12-11 19:52:43 -0600

    Update ibv_helper.h

--- ibv_helper.h
+++ ibv_helper.h
@@ -36,9 +36,9 @@
 #include <sys/types.h>
 #include <dirent.h>
 
-enum ibv_gid_type {
-        IBV_GID_TYPE_IB_ROCE_V1,
-        IBV_GID_TYPE_ROCE_V2,
+enum _ibv_gid_type {
+        _IBV_GID_TYPE_IB_ROCE_V1,
+        _IBV_GID_TYPE_ROCE_V2,
 };
 
 static int ibv_read_sysfs_file(const char *dir, const char *file,
@@ -86,7 +86,7 @@ static int ibv_read_sysfs_file(const char *dir, const char *file,
 #define V1_TYPE "IB/RoCE v1"
 #define V2_TYPE "RoCE v2"
 static int ibv_query_gid_type(struct ibv_context *context, uint8_t port_num,
-                unsigned int index, enum ibv_gid_type *type)
+                unsigned int index, enum _ibv_gid_type *type)
 {
         char name[32];
         char buff[11];
@@ -107,7 +107,7 @@ static int ibv_query_gid_type(struct ibv_context *context, uint8_t port_num,
 			/* In IB, this file doesn't exist and the kernel sets
 			 * errno to -EINVAL.
 			 */
-			*type = IBV_GID_TYPE_IB_ROCE_V1;
+			*type = _IBV_GID_TYPE_IB_ROCE_V1;
 			return 0;
 		}
 		if (asprintf(&dir_path, "%s/%s/%d/%s/",
@@ -122,7 +122,7 @@ static int ibv_query_gid_type(struct ibv_context *context, uint8_t port_num,
 				 * we have an old kernel and all GIDs are
 				 * IB/RoCE v1
 				 */
-				*type = IBV_GID_TYPE_IB_ROCE_V1;
+				*type = _IBV_GID_TYPE_IB_ROCE_V1;
 			else
 				return -1;
 		} else {
@@ -132,9 +132,9 @@ static int ibv_query_gid_type(struct ibv_context *context, uint8_t port_num,
 		}
 	} else {
                 if (!strcmp(buff, V1_TYPE)) {
-                        *type = IBV_GID_TYPE_IB_ROCE_V1;
+                        *type = _IBV_GID_TYPE_IB_ROCE_V1;
                 } else if (!strcmp(buff, V2_TYPE)) {
-                        *type = IBV_GID_TYPE_ROCE_V2;
+                        *type = _IBV_GID_TYPE_ROCE_V2;
                 } else {
                         errno = ENOTSUP;
                         return -1;
@@ -145,9 +145,9 @@ static int ibv_query_gid_type(struct ibv_context *context, uint8_t port_num,
 }
 
 int ibv_find_sgid_type(struct ibv_context *context, uint8_t port_num,
-		enum ibv_gid_type gid_type, int gid_family)
+		enum _ibv_gid_type gid_type, int gid_family)
 {
-        enum ibv_gid_type sgid_type = 0;
+        enum _ibv_gid_type sgid_type = 0;
         union ibv_gid sgid;
         int sgid_family = -1;
         int idx = 0;
